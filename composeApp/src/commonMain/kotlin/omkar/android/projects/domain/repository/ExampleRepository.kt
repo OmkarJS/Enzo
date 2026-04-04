fa12a0437ee112225c8af20ab70d46ca1be3419f
Omkar J Suvarna <omkarjsuvarna@gmail.com>
2026-04-04 14:09:04 +0530

    Changes: - Removed unused classes

--- composeApp/src/commonMain/kotlin/omkar/android/projects/domain/repository/ExampleRepository.kt
+++
@@ -1,8 +0,0 @@
-package omkar.android.projects.domain.repository
-
-import omkar.android.projects.data.model.ExampleResponse
-import omkar.android.projects.app.utils.ApiResponseWrapper
-
-interface ExampleRepository {
-    suspend fun fetchExample(): ApiResponseWrapper<ExampleResponse>
-}
--- composeApp/src/commonMain/kotlin/omkar/android/projects/domain/repository/SensoryEncoder.kt
+++
@@ -1,8 +0,0 @@
-package omkar.android.projects.domain.repository
-
-import omkar.android.projects.domain.model.SDR
-import omkar.android.projects.domain.model.sensory.SensoryInput
-
-interface SensoryEncoder {
-    fun encode(sensoryInput: SensoryInput): SDR
-}
